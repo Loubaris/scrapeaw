5350cf77cad242d1b05ac38cfa2111e1bfa5a16b
Loubaris <adamou.loubaris@gmail.com>
2023-08-11 12:40:14 +0200

    Update scrapaw.py

--- scrapaw.py
+++ scrapaw.py
@@ -1,31 +1,30 @@
-################################################################################
-################################################################################
-####																	   #####
-####  Copyright (C) 2021-2022, Loubaris									   #####
-####																	   #####
-####  This program is free software; you can redistribute it and/or modify #####
-####  it under the terms of the GNU General Public License as published by #####
-####  the Free Software Foundation; either version 2 of the License, or	   #####
-####  (at your option) any later version.								   #####
-####																	   #####
-####  This program is distributed in the hope that it will be useful,	   #####
-####  but WITHOUT ANY WARRANTY; without even the implied warranty of	   #####
-####  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		   #####
-####  GNU General Public License for more details.						   #####
-####																	   #####
-####  You must obey the GNU General Public License. If you will modify	   #####
-####  this file(s), you may extend this exception to your version		   #####
-####  of the file(s), but you are not obligated to do so.  If you do not   #####
-####  wish to do so, delete this exception statement from your version.    #####
-####  If you delete this exception statement from all source files in the  #####
-####  program, then also delete it here.								   #####
-####																	   #####
-####  Contact:															   #####
-#### 																	   #####
-####		  Mail: adamou.loubaris@gmail.com							   #####
-####																	   #####
-################################################################################
-################################################################################
+"""
+
+									   
+  Copyright (C) 2021-2022, Loubaris					   
+									   
+  This program is free software; you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by 
+  the Free Software Foundation; either version 2 of the License, or	   
+  (at your option) any later version.				   
+									   
+  This program is distributed in the hope that it will be useful,	   
+  but WITHOUT ANY WARRANTY; without even the implied warranty of	   
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+  GNU General Public License for more details.			   
+																	   
+  You must obey the GNU General Public License. If you will modify	   
+  this file(s), you may extend this exception to your version		   
+  of the file(s), but you are not obligated to do so.  If you do not   
+  wish to do so, delete this exception statement from your version.    
+  If you delete this exception statement from all source files in the  
+  program, then also delete it here.								   
+																	   
+  Contact:															   
+ 																	   
+		  Mail: adamou.loubaris@gmail.com							  
+																	   
+"""
 
 
 import os
